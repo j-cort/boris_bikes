3442cebba94337096f3942682953488b186a035d
j-cort <81960011+j-cort@users.noreply.github.com>
2021-11-16 16:24:11 +0000

    finish first user story

--- lib/dockingstation.rb
+++ lib/dockingstation.rb
@@ -1,5 +1,8 @@
+require_relative "./bike.rb"
+
 class DockingStation
 
     def release_bike
+        return Bike.new
     end
 end
--- spec/boris_bikes_spec.rb
+++ spec/boris_bikes_spec.rb
@@ -4,4 +4,10 @@ describe DockingStation do
     it "Docking Station has method of releasing a bike" do
         expect(subject).to respond_to :release_bike
     end
+    it "Docking station creates an new bike when release_bike is called" do
+    expect(subject.release_bike).to be_kind_of(Bike)
+    end
+    it 'returns true when asked if bike is working' do
+    expect(subject.release_bike.working?).to eq (true)
+    end
 end
